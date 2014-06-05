e287a0b3e79b2d8a0494bf2393101171a15cb2df
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2014-06-04 21:26:42 -0700

    Ready to label data!

--- TreatmentAdherence/src/main/java/edu/arizona/adherence/MainActivity.java
+++ TreatmentAdherence/src/main/java/edu/arizona/adherence/MainActivity.java
@@ -1,79 +1,194 @@
 package edu.arizona.adherence;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.ubhave.datahandler.loggertypes.AbstractDataLogger;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends Activity {
 
+    private static final String TAG_LABEL = "Label";
+
+    private static final String TAG_ACTIVITY = "activity";
+    private static final String TAG_EVENT = "event";
+    private static final String TAG_ACTIVITY_START = "start";
+    private static final String TAG_ACTIVITY_STOP = "stop";
+    private final static String TAG_TIMESTAMP = "timestamp";
+    private final static String TAG_LOCAL_TIME = "localTime";
+
+    private static final String PREFS_NAME = "TreatmentPrefs";
+
     private boolean isSensing;
-    private SensingListener sensor;
+    private SharedPreferences mSettings;
+    private Button sensingButton;
+    private Button activityButton;
+    private Spinner mActivities;
+    private boolean isPerforming; // Check whether the user is performing the activity
+
+    private AbstractDataLogger dataLogger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         isSensing = false;
+        isPerforming = false;
 
-        AbstractDataLogger dataLogger = new SensingStoreOnlyLogger(this);
-        sensor = new SensingListener(this, dataLogger);
+        mSettings = getSharedPreferences(PREFS_NAME, 0);
+        sensingButton = (Button) findViewById(R.id.sensing_button);
+        activityButton = (Button) findViewById(R.id.activity_button);
+
+        mActivities = (Spinner) findViewById(R.id.spinner_activity);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.activity_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mActivities.setAdapter(adapter);
+
+        dataLogger = new SensingStoreOnlyLogger(this);
+
+        //AbstractDataLogger dataLogger = new SensingStoreOnlyLogger(this);
+        //sensor = new SensingListener(this, dataLogger);
+        //Intent startServiceIntent = new Intent(this, SensingService.class);
+        //startService(startServiceIntent);
     }
 
     @Override
     protected void onPause()
     {
         super.onPause();
-        if (isSensing)
-        {
-            switchSensing((Button) findViewById(R.id.sensing_button));
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        editor.clear();
+        editor.putBoolean("sensing", isSensing);
+        editor.putBoolean("performing", isPerforming);
+        editor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        isSensing = mSettings.getBoolean("sensing", false);
+        isPerforming = mSettings.getBoolean("performing", false);
+
+        Log.d("isSensing", String.valueOf(isSensing));
+        Log.d("isPerforming", String.valueOf(isPerforming));
+        switchButtonText();
+
+        if(isPerforming) {
+            activityButton.setText(R.string.button_stopActivity);
+            sensingButton.setEnabled(false);
+        } else {
+            activityButton.setText(R.string.button_startActivity);
+            sensingButton.setEnabled(true);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSettings.edit().clear();
     }
 
     /*
-	 * Listener for UI button
-	 */
+             * Listener for UI button
+             */
     public void switchSensing(final View view)
     {
         isSensing = !isSensing;
-        if (switchSensing())
-        {
-            switchButtonText((Button) view);
+        if (switchSensing()) {
+            switchButtonText();
         }
     }
 
-    private void switchButtonText(final Button sensingButton)
+    private void switchButtonText()
     {
         if (isSensing)
         {
             sensingButton.setText(R.string.button_stopSensing);
+            activityButton.setEnabled(true);
         }
         else
         {
             sensingButton.setText(R.string.button_startSensing);
+            activityButton.setEnabled(false);
         }
     }
 
     private boolean switchSensing()
     {
-        if (sensor != null)
-        {
-            if (isSensing)
-            {
-                return sensor.startSensing();
-            }
-            else
-            {
-                return sensor.stopSensing();
-            }
+        if (isSensing) {
+            Intent startServiceIntent = new Intent(this, SensingService.class);
+            startService(startServiceIntent);
+        } else {
+            Intent stopServiceIntent = new Intent(this, SensingService.class);
+            stopService(stopServiceIntent);
         }
-        else
-        {
-            return false;
+        return true;
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private String localTime()
+    {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss zZ");
+        return dateFormat.format(calendar.getTime());
+    }
+
+    //
+    public void switchActivity(final View view) {
+        if (!isPerforming) {
+            // TODO Save the start time of activity
+            JSONObject json = new JSONObject();
+            try {
+                json.put(TAG_ACTIVITY, mActivities.getSelectedItem().toString());
+                json.put(TAG_EVENT, TAG_ACTIVITY_START);
+                json.put(TAG_TIMESTAMP, System.currentTimeMillis());
+                json.put(TAG_LOCAL_TIME, localTime());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            dataLogger.logExtra(TAG_LABEL, json);
+
+            activityButton.setText(R.string.button_stopActivity);
+            sensingButton.setEnabled(false);
+            Toast toast = Toast.makeText(this, "Activity Started", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            // TODO Save the stop time of activity
+            JSONObject json = new JSONObject();
+            try {
+                json.put(TAG_ACTIVITY, mActivities.getSelectedItem().toString());
+                json.put(TAG_EVENT, TAG_ACTIVITY_STOP);
+                json.put(TAG_TIMESTAMP, System.currentTimeMillis());
+                json.put(TAG_LOCAL_TIME, localTime());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            dataLogger.logExtra(TAG_LABEL, json);
+
+            activityButton.setText(R.string.button_startActivity);
+            sensingButton.setEnabled(true);
+            Toast toast = Toast.makeText(this, "Activity Stopped", Toast.LENGTH_SHORT);
+            toast.show();
         }
+        isPerforming = !isPerforming;
     }
 }
--- TreatmentAdherence/src/main/java/edu/arizona/adherence/SensingService.java
+++ TreatmentAdherence/src/main/java/edu/arizona/adherence/SensingService.java
@@ -1,16 +1,25 @@
 package edu.arizona.adherence;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
+
+import com.ubhave.datahandler.loggertypes.AbstractDataLogger;
+import com.ubhave.sensormanager.ESException;
+import com.ubhave.sensormanager.ESSensorManager;
 
 /**
  *  This service collect data from sensors and save locally and remotely
  */
 public class SensingService extends Service {
-    int mStartMode;
-    IBinder mBinder;
-    boolean mAllowRebind;
+    private static final int ONGOING_NOTIFICATION_ID = 192;
+    //private IBinder mBinder;
+    //private boolean mAllowRebind;
+    private ESSensorManager sensorManager;
+    private SensingListener sensor;
 
     public SensingService() {
     }
@@ -18,23 +27,52 @@ public class SensingService extends Service {
     @Override
     public void onCreate() {
         // The service is being created
+        try {
+            sensorManager = ESSensorManager.getSensorManager(this);
+        } catch (ESException e) {
+            e.printStackTrace();
+        }
+
+        AbstractDataLogger dataLogger = new SensingStoreOnlyLogger(this);
+        sensor = new SensingListener(this, dataLogger);
+
+        // Device ID
+        //TelephonyManager mngr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        //mngr.getDeviceId();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // The service is starting, due to a call to startService()
-        return mStartMode;
+        sensor.startSensing();
+        Toast toast = Toast.makeText(this, "Sensing Service Started", Toast.LENGTH_LONG);
+        toast.show();
+
+        // Put service in foreground
+        Notification notification = new Notification(
+                R.drawable.treatment,
+                getText(R.string.service_notification),
+                System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(this, getText(R.string.notification_title),
+                getText(R.string.notification_message), pendingIntent);
+        startForeground(ONGOING_NOTIFICATION_ID, notification);
+
+        return START_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // A client is binding to the service with bindService()
+        sensor.startSensing();
         return mBinder;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
         // All clients have unbound with unbindService()
+        sensor.stopSensing();
         return mAllowRebind;
     }
 
@@ -47,5 +85,9 @@ public class SensingService extends Service {
     @Override
     public void onDestroy() {
         // The service is no longer used and is being destroyed
+        sensor.stopSensing();
+        stopForeground(true);
+        Toast toast = Toast.makeText(this, "Sensing Service Started", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
